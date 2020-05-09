a05c90e6b069a74300dbf771ba95a134e6534670
Nirav Patel <niravkpatel28@gmail.com>
2020-05-09 23:20:21 +0530

    Updates

---
+++ precache-manifest.c349c8b86b94bc86fc654c213ef79255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555f2f712950dcc69fbe462b2f38cc9a",
+    "url": "/react-routing-user-blogs-v2/index.html"
+  },
+  {
+    "revision": "51a9355e59ebaff645a4",
+    "url": "/react-routing-user-blogs-v2/static/css/main.7a893241.chunk.css"
+  },
+  {
+    "revision": "d98bb0b365d95379428f",
+    "url": "/react-routing-user-blogs-v2/static/js/2.10bc9eee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-routing-user-blogs-v2/static/js/2.10bc9eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a9355e59ebaff645a4",
+    "url": "/react-routing-user-blogs-v2/static/js/main.1a9af221.chunk.js"
+  },
+  {
+    "revision": "f13be038b68c337b4794",
+    "url": "/react-routing-user-blogs-v2/static/js/runtime-main.819faf07.js"
+  }
+]);
